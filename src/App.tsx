88ab0cb631cef81074af934bb993f7ac6f7204c9
Roman Kulikov <kulikovroman746@gmail.com>
2022-07-15 00:30:06 +0300

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import Empty from "./components/Empty";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Empty title='content' />} />
+        <Route path="/type" element={<Empty title='type' />} />
+        <Route path="/product" element={<Empty title='product' />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
