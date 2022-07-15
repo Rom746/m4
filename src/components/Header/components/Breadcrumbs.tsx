02b81b16d40156af951e45d59318d0aedfa5fbfb
Roman Kulikov <kulikovroman746@gmail.com>
2022-07-15 23:46:11 +0300

    took out the routes

--- src/components/Header/components/Breadcrumbs.tsx
+++ src/components/Header/components/Breadcrumbs.tsx
@@ -1,26 +1,16 @@
 import React, { FC } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
+import { titleRoutes } from 'utils/titleRoutes';
 
 const Breadcrumbs: FC = () => {
     const { pathname } = useLocation();
     const crumbs = pathname.split("/").filter((el) => el);
 
-    interface routesProps {
-        [index: string]: string;
-    }
-
-    const routes: routesProps = {
-        '/': 'Главная',
-        'product': 'Продукты',
-        'type': 'Типы продуктов',
-        'cart': 'Корзина'
-    }
-
-    const crumbItem = (url: string, title: string) => {
+    const crumbItem = (url: string) => {
         return (
             <li className='breadcrumbs__item'>
                 <NavLink to={url} key={url} className='breadcrumbs__link'> 
-                    {routes[title]}
+                    {titleRoutes[url]}
                 </NavLink>
             </li>
         )
@@ -31,14 +21,14 @@ const Breadcrumbs: FC = () => {
             const routeTo = `/${crumbs.slice(0, index + 1).join("/")}`;
             const isLast = index === crumbs.length - 1;
             return isLast ? (
-                <span key={routeTo}>{routes[crumb]}</span>
-            ) : crumbItem(routeTo, crumb);
+                <span key={routeTo}>{titleRoutes[routeTo]}</span>
+            ) : crumbItem(routeTo);
         })
     }
     return (
         <nav className='breadcrumbs'>
             <ul className="breadcrumbs__list">
-                {crumbItem('/', '/')}
+                {crumbItem('/')}
                 {crumbsList()}
             </ul>
 
